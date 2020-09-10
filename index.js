c9130676383b701d799e9e8c4b008e9564fbb23a
Alex Costan <acostan.con@orangetheory.com>
2020-09-10 17:19:56 -0400

    added route to images

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const server = http.createServer((req, res) => {
   const pathName = url.parse(req.url, true).pathname
   const id = url.parse(req.url, true).query.id
 
-  console.log(url.parse(req.url, true))
-
-  // Products Overview
+  // PRODUCTS OVERVIEW
   if (pathName === "/products" || pathName === "/") {
     res.writeHead(200, { "Content-type": "text/html" })
 
@@ -20,19 +18,24 @@ const server = http.createServer((req, res) => {
       "utf-8",
       (err, data) => {
         let overviewOutput = data
+
         fs.readFile(
           `${__dirname}/templates/template-card.html`,
           "utf-8",
           (err, data) => {
-            const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('')
-            overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput)
-            res.end(data)
+            const cardsOutput = laptopData
+              .map((el) => replaceTemplate(data, el))
+              .join("")
+            overviewOutput = overviewOutput.replace("{%CARDS%}", cardsOutput)
+
+            res.end(overviewOutput)
           }
         )
       }
     )
   }
-  // Laptop Detail
+
+  // LAPTOP DETAIL
   else if (pathName === "/laptop" && id < laptopData.length) {
     res.writeHead(200, { "Content-type": "text/html" })
 
@@ -45,18 +48,29 @@ const server = http.createServer((req, res) => {
         res.end(output)
       }
     )
-  } else {
-    res.writeHead(200, { "Content-type": "text/html" })
+  }
+
+  // IMAGES
+  else if (/\.(jpg|jpeg|png|gif)$/i.test(pathName)) {
+    fs.readFile(`${__dirname}/data/img${pathName}`, (err, data) => {
+      res.writeHead(200, { "Content-type": "image/jpg" })
+      res.end(data)
+    })
+  }
+
+  // URL NOT FOUND
+  else {
+    res.writeHead(404, { "Content-type": "text/html" })
     res.end("URL was not found on the server!")
   }
 })
 
 server.listen(3000, "127.0.0.1", () => {
-  console.log("Listening for for requests")
+  console.log("Listening for requests now")
 })
 
 function replaceTemplate(originalHtml, laptop) {
-  let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName)
+  let output = originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName)
   output = output.replace(/{%IMAGE%}/g, laptop.image)
   output = output.replace(/{%PRICE%}/g, laptop.price)
   output = output.replace(/{%SCREEN%}/g, laptop.screen)
@@ -64,5 +78,6 @@ function replaceTemplate(originalHtml, laptop) {
   output = output.replace(/{%STORAGE%}/g, laptop.storage)
   output = output.replace(/{%RAM%}/g, laptop.ram)
   output = output.replace(/{%DESCRIPTION%}/g, laptop.description)
+  output = output.replace(/{%ID%}/g, laptop.id)
   return output
 }
